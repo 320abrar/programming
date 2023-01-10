a358a83b2916b7ff036aa08b6646f9dc132bcae0
320abrar <121958214+320abrar@users.noreply.github.com>
2023-01-10 22:14:42 +0500

    Add files via upload

---
+++ task7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void tochek(char alphabat);
+main()
+{
+    char alphabat;
+    cout<<"enter the alphabat to chek";
+    cin>>alphabat;
+    tochek(alphabat);
+
+}
+void tochek(char alphabat)
+{
+    if(alphabat='a');
+    {
+    cout<<"user has entered small case alphabat";
+}
+if(alphabat=='A')
+{
+    cout<<"user has entered uper case alphabat";
+}
+}
