bc5ada98e63cfc71911fd674f57adfe30b956e60
320abrar <121958214+320abrar@users.noreply.github.com>
2023-01-10 22:11:58 +0500

    Add files via upload

--- task1.cpp
+++ task1.cpp
@@ -1,11 +1,34 @@
 #include<iostream>
+#include<windows.h>
 using namespace std;
-int x=10;
-void myfunction()
-{
-    int x=20;
-}
+int add(int number1,int number2);
+float division(int number1,int number2);
 main()
 {
-    cout<<"value of x is:"<<x;                                      
+system("cls");
+int number1;
+int number2;
+float result;
+cout<<"enter first no";
+cin>>number1;
+cout<<"enter second no";
+cin>>number2;
+result=add(number1,number2);
+cout<<result<<endl;
+result=division(number1,number2);
+cout<<result;
+}
+int add(int number1,int number2)
+{
+int sum;
+sum=number1+number2;
+
+return  sum;
+}
+float division(int number1,int number2)
+{
+float divide;
+divide=number1/number2;
+
+return divide;
 }
